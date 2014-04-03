ed4eb54ec14e1b3505b8217f3891af9e1f3e162b
yourpleasure <lixu19890724@163.com>
2014-04-03 22:46:47 +0000

    Add 1081

--- 1081/Main.c
+++ 1081/Main.c
@@ -1,33 +1,62 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
 const int mod = 10000;
+int T[2][2];
+void mul(int A[2][2], int B[2][2]){
+	int S[2][2];
+
+	S[0][0] = A[0][0] * B[0][0] + A[0][1] * B[1][0];
+	S[0][1] = A[0][0] * B[0][1] + A[0][1] * B[1][1];
+	S[1][0] = A[1][0] * B[0][0] + A[1][1] * B[1][0];
+	S[1][1] = A[1][0] * B[0][1] + A[1][1] * B[1][1];
+
+	A[0][0] = S[0][0] % mod;
+	A[0][1] = S[0][1] % mod;
+	A[1][0] = S[1][0] % mod;
+	A[1][1] = S[1][1] % mod;
+}
+
+void bi(int A[2][2], long long n)
+{
+	if(n == 1) return;
+	if(n % 2){
+		bi(A, n-1);
+		mul(A, T);
+	}
+	else{
+		bi(A, n/2);
+		mul(A, A);
+	}
+}
+
 int main()
 {
-	int p, q;
-	int k;
-	int i;
-	int A[100002];
+	long long p, q;
+	long long k;
+	long long a0, a1;
 
-	memset(A, '\0', sizeof(A));
-	while(scanf("%d%d%d%d%d", A, A+1, &p, &q, &k) != EOF){
-		A[0] %= mod;
-		A[1] %= mod;
+	int p0, q0;
+	int A[2][2];
+	int a00, a11;
 
-		for(i = 2; i <= k; i++){
-			A[i] = p * A[i-1] + q * A[i-2];
-			A[i] %= mod;
-			if(i == 100001){
-				A[0] = A[100000];
-				A[1] = A[100001];
-				i = 1;
-				k -= 100000;
-			}
+	while(scanf("%Ld%Ld%Ld%Ld%Ld", &a0, &a1, &p, &q, &k) != EOF){
+		a00 = (int)(a0 % mod);
+		a11 = (int)(a1 % mod);
+		p0 = (int)(p % mod);
+		q0 = (int)(q % mod);
+		if(k == 0) printf("%d\n", a00);
+		else if(k == 1) printf("%d\n", a11);
+		else{
+			A[0][0] = T[0][0] = p0;
+			A[0][1] = T[0][1] = q0;
+			A[1][0] = T[1][0] = 1;
+			A[1][1] = T[1][1] = 0;
+			bi(A, k-1);
+			A[0][0] = A[0][0] * a11 + A[0][1] * a00;
+			A[0][0] %= mod;
+			printf("%d\n", A[0][0]);
 		}
-
-		printf("%d\n", A[k]);
-		memset(A, '\0', sizeof(A));
 	}
 	exit(0);
 }
